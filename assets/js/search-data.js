3f089d2f7c0f089f5f866888dda79018fdb9159e
otroshi <72525669+otroshi@users.noreply.github.com>
2026-03-03 09:13:23 +0000

    Deploying to gh-pages from @ foundgen-bio/cvpr2026@ec92fee81b135366d140f09b39cae6916efb5297 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -47,8 +47,8 @@ ninja.data = [{
           section: "News",},{id: "news-important-dates-are-announced-and-the-submission-is-now-open-see-call-for-papers",
           title: 'Important dates are announced and the submission is now open - see Call...',
           description: "",
-          section: "News",},{id: "news-submission-deadline-is-extended-to-march-8",
-          title: 'Submission deadline is extended to March 8!',
+          section: "News",},{id: "news-submission-deadline-is-extended-to-march-7",
+          title: 'Submission deadline is extended to March 7!',
           description: "",
           section: "News",},{
       id: 'light-theme',
